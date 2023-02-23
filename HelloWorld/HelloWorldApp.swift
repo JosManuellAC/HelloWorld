ba740478d9895aa0ae02cba66e5f3d5f333f283e
JosManuellAC <josmanuel.alonsoc@gmail.com>
2023-02-23 15:50:03 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by José Manuel Alonso on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
